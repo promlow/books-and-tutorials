93d244cb6920c4a972feb1d4d946722ae68ab73b
Paul Romlow <paul.romlow@tdstelecom.com>
2013-12-04 13:29:29 -0600

    finished chapter 4

--- ActorsInScala/Chapter4/ChatRoom.scala
+++ ActorsInScala/Chapter4/ChatRoom.scala
@@ -12,10 +12,10 @@ case class Post(msg: String)
 case class UserPost(user: User, post:Post)
 
 class ChatRoom extends Actor {
+  var session = Map.empty[User, Actor]
 
   def act() {
     // the actor's behavior
-    var session = Map.empty[User, Actor]
     while (true)
     {
       receive {
@@ -25,14 +25,22 @@ class ChatRoom extends Actor {
             actor {
 	      while (true) {
                 self.receive {
-                  case Post(msg) => subscriber ! Post(msg)
+                  case Post(msg) => 
+                    subscriber ! Post(msg)
+                    println(user +" received:"+ msg)
                 }
               }
             }
           session = session + (user -> sessionUser)
 	  reply("Subscribed " + user)
-        case Unsubscribe(user) => //handle unsubscriptions
-        case UserPost(user, post) => //handle user posts
+        case Unsubscribe(user) =>
+	  session = session - user
+          reply("Unsubscribed " + user)
+        case UserPost(user, post) => 
+	  println(user +" posted:"+ post)
+          for (key <- session.keys; if key != user){
+            session(key) ! post
+          }
       }
     }
   }   
@@ -45,4 +53,25 @@ chatRoom.start()
 chatRoom !? Subscribe(User("Bob")) match {
   case response: String => println(response)
 }
+chatRoom ! Subscribe(User("Me"))
+
+chatRoom ! UserPost(User("Me"), Post("Hi Bob"))
+
+chatRoom ! UserPost(User("Bob"), Post("Hi you"))
+
+for(p <- 1 to 100){ //Should arrive out of order, some anyway
+  chatRoom ! UserPost(User("Me"), Post("Me"+p))
+  chatRoom ! UserPost(User("Bob"), Post("Bob"+p))
+}
+
+//Future
+val future = chatRoom !! Unsubscribe(User("Bob"))
+
+val waiting = List("waiting... " * 10)
+waiting.foreach(w => print(w))
+println()
+
+println(future())
+
+chatRoom !? Unsubscribe(User("Me"))
 
