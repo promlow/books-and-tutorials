0d2148e6b01a0805dde35d1dfba8a86ef554133e
Paul Romlow <paul.romlow@tdstelecom.com>
2013-12-05 15:30:55 -0600

    better error message

--- ActorsInScala/Chapter6/trapExit.scala
+++ ActorsInScala/Chapter6/trapExit.scala
@@ -1,5 +1,6 @@
 import scala.actors.Exit
 import scala.actors.Actor._
+import scala.actors.UncaughtException
 
 val a = actor {
   react {
@@ -15,7 +16,17 @@ val b = actor {
   link(a)
   a ! 'start
   react {
-    case Exit(from, reason) if from == a =>
-      println("Actor 'a' terminated because of " + reason)
+    //case Exit(from, reason) if from == a =>
+    case Exit(from, UncaughtException(_, _, _, _, cause))
+        if from == a =>
+      println("Actor 'a' terminated because of " + cause)
   }
 }
+/**
+ * Change from: case Exit(from, reason) if from == a =>
+ * to: case Exit(from, UncaughtException(_, _, _, _, cause))
+        if from == a =>
+ * results in output change from:
+ * Actor 'a' terminated because of UncaughtException(scala.actors.Actor$$anon$1@1742700,Some('start),Some(scala.actors.Actor$$anon$1@16c79d7),java.lang.Exception: Error!)
+ * to: Actor 'a' terminated because of java.lang.Exception: Error!
+ **/
