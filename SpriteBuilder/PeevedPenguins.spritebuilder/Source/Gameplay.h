b1bc837b97b23b7e538bd88861856fe6a5eaf0ec
Paul Romlow <paul@romlow.com>
2014-03-12 20:57:26 -0500

    Gameplay scene

---
+++ SpriteBuilder/PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Paul Romlow on 3/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
