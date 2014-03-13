3b124a5312e87db94d2549494dd8d93753b9d66b
Paul Romlow <paul@romlow.com>
2014-03-12 20:39:35 -0500

    menu

--- SpriteBuilder/PeevedPenguins.spritebuilder/Source/MainScene.m
+++ SpriteBuilder/PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -10,4 +10,10 @@
 
 @implementation MainScene
 
+
+- (void)play
+{
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
+}
 @end
