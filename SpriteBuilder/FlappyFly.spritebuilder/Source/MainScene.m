2d3b93c7ea85ef1ef7d68d70340270bfedb65e95
Paul Romlow <paul@romlow.com>
2014-03-26 20:30:15 -0500

    Spawning new obstacles when old ones leave the screen

--- SpriteBuilder/FlappyFly.spritebuilder/Source/MainScene.m
+++ SpriteBuilder/FlappyFly.spritebuilder/Source/MainScene.m
@@ -8,6 +8,8 @@
 
 #import "MainScene.h"
 
+static const CGFloat firstObstaclePosition = 280.0;
+static const CGFloat distanceBetweenObstacles = 160.0;
 static const CGFloat scrollSpeed = 80.0;
 
 @implementation MainScene
@@ -17,16 +19,33 @@ static const CGFloat scrollSpeed = 80.0;
     CCNode *_ground1;
     CCNode *_ground2;
     NSArray *_grounds;
+    NSTimeInterval _sinceTouch;
+    NSMutableArray *_obstacles;
 }
 
 - (void)didLoadFromCCB
 {
     _grounds = @[_ground1, _ground2];
+    _obstacles = [NSMutableArray array];
+    [self spawnNewObstacle];
+    [self spawnNewObstacle];
+    [self spawnNewObstacle];
+    
     self.userInteractionEnabled = YES;
 }
 
 - (void)update:(CCTime)delta
 {
+    _sinceTouch += delta;
+    _hero.rotation = clampf(_hero.rotation, -30.f, 90.f);
+    if (_hero.physicsBody.allowsRotation)
+    {
+        float angularVelocity = clampf(_hero.physicsBody.angularVelocity, -2.0, 1.0);
+        _hero.physicsBody.angularVelocity = angularVelocity;
+    }
+    if ((_sinceTouch > 0.5f)) {
+        [_hero.physicsBody applyAngularImpulse:-40000.0 * delta];
+    }
     _hero.position = ccp(_hero.position.x + delta * scrollSpeed, _hero.position.y);
     _physicsNode.position = ccp(_physicsNode.position.x - (scrollSpeed *delta), _physicsNode.position.y);
     for (CCNode *ground in _grounds)
@@ -40,11 +59,45 @@ static const CGFloat scrollSpeed = 80.0;
     // clamp velocity
     float yVelocity = clampf(_hero.physicsBody.velocity.y, -1 * MAXFLOAT, 200);
     _hero.physicsBody.velocity = ccp(0, yVelocity);
+    
+    NSMutableArray *offScreenObstacles = nil;
+    for (CCNode *obstacle in _obstacles) {
+        CGPoint obstacleWorldPosition = [_physicsNode convertToWorldSpace:obstacle.position];
+        CGPoint obstacleScreenPosition = [self convertToNodeSpace:obstacleWorldPosition];
+        if (obstacleScreenPosition.x < -obstacle.contentSize.width) {
+            if (!offScreenObstacles) {
+                offScreenObstacles = [NSMutableArray array];
+            }
+            [offScreenObstacles addObject:obstacle];
+        }
+    }
+    for (CCNode *obstacleToRemove in offScreenObstacles) {
+        [obstacleToRemove removeFromParent];
+        [_obstacles removeObject:obstacleToRemove];
+        // for each removed obstacle, add a new one
+        [self spawnNewObstacle];
+    }
 }
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
     [_hero.physicsBody applyImpulse:ccp(0, 400.0)];
+    [_hero.physicsBody applyAngularImpulse:10000.0];
+    _sinceTouch = 0.0;
+}
+
+- (void)spawnNewObstacle
+{
+    CCNode *previousObstacle = [_obstacles lastObject];
+    CGFloat previousObstacleXPosition = previousObstacle.position.x;
+    if (!previousObstacle) {
+        // this is the first obstacle
+        previousObstacleXPosition = firstObstaclePosition;
+    }
+    CCNode *obstacle = [CCBReader load:@"Obstacle"];
+    obstacle.position = ccp(previousObstacleXPosition + distanceBetweenObstacles, 0);
+    [_physicsNode addChild:obstacle];
+    [_obstacles addObject:obstacle];
 }
 
 @end
